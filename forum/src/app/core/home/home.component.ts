c678109af31c2621f01164e1b2e7dbf150bce5f6
ganchoganev <gancho.gan@gmail.com>
2022-12-13 01:04:59 +0200

    13.12.22

--- forum/src/app/core/home/home.component.ts
+++ forum/src/app/core/home/home.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  get isLoggedIn() {
+    return this.authServie.isLoggedIn;
   }
+
+  constructor(private authServie: AuthService) { }
 
 }
